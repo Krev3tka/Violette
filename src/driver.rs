4f9806b80b96f6ec38de006ab8f8062d13b45262
Krev3tka <179160528+Krev3tka@users.noreply.github.com>
2026-08-03 12:39:51 +0300

    feat: CLI driver — violette build/run

---
+++ src/driver.rs
@@ -0,0 +1,80 @@
+use std::{env, fs};
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+use std::process::Command;
+use crate::codegen::codegen::Codegen;
+use crate::lexer::lexer::Lexer;
+use crate::parser::parser::Parser;
+use crate::typechecker::checker::Checker;
+
+pub fn find_cc() -> Option<String> {
+    if let Ok(cc) = std::env::var("CC") {
+        return Some(cc)
+    }
+
+    for cand in ["cc", "clang", "gcc"] {
+        if Command::new(cand).arg("--version").output().is_ok() {
+            return Some(cand.to_string())
+        }
+    }
+
+    None
+}
+
+pub fn compile(command: &str, file: &str) {
+    let Some(compiler) = find_cc() else { return println!("Didn't find any C compilers") };
+
+    let input = fs::read_to_string(file).expect("Failed to read file");
+
+    let lexer = Lexer::new(&input);
+    let mut parser = Parser::new(lexer);
+
+    let ast = match parser.parse_program() {
+        Ok(prg) => prg,
+        Err(e) =>  return println!("Parse error: {}", e),
+    };
+
+    let mut checker = Checker::default();
+
+    checker.check_program(&ast);
+
+    if !checker.errors.is_empty() {
+        return println!("Type errors: {:?}", checker.errors)
+    }
+
+    let mut codegen = Codegen::new();
+
+    let code = match codegen.emit_program(ast) {
+        Ok(out) => out,
+        Err(e) => return println!("Codegen error: {:?}", e)
+    };
+
+    let c_path = env::temp_dir().join("vio_out.c");
+
+    let mut c_file = File::create(&c_path)
+        .expect("Failed to create temporary C file");
+
+    c_file.write_all(code.as_bytes()).expect("Failed to write to temporary C file");
+
+    let out = Path::new(file).file_stem().unwrap().to_str().unwrap();
+
+    let status = Command::new(&compiler)
+        .args(["-std=c99", "-O3", c_path.to_str().unwrap(), "-o", out])
+        .status()
+        .map_err(|e| e.to_string())
+        .expect("Failed to compile");
+
+     if !status.success() {
+         println!("{} failed", compiler);
+         return
+     }
+
+    if command == "run" {
+        let status = Command::new(format!("./{}", out)).status().expect("Failed to execute");
+
+        if !status.success() {
+            println!("{} failed", compiler);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 #![allow(clippy::module_inception)]
 
+use std::env;
 use crate::codegen::codegen::Codegen;
+use crate::driver::compile;
 use crate::lexer::lexer::Lexer;
 use crate::parser::parser::Parser;
-use crate::typechecker::checker::Checker;
+use crate::typechecker::checker::{Checker, TypeError};
 
 mod codegen;
 mod lexer;
@@ -12,95 +14,18 @@ mod parser;
 #[cfg(test)]
 mod tests;
 mod typechecker;
-
-fn check(input: &str) -> String {
-    let lexer = Lexer::new(input);
-
-    let mut parser = Parser::new(lexer);
-    let ast = parser.parse_program().unwrap();
-
-    let mut checker = Checker::default();
-
-    checker.check_program(&ast);
-
-    let codegen = Codegen::new();
-
-    codegen.out
-}
+mod driver;
 
 fn main() {
-    let inputs = vec![
-        "package main
+    let args: Vec<String> = env::args().collect();
 
-        let x = true != 5",
-        "package double
-
-        fun double(x: int) [int] {
-            return x * 2
-        }
-
-        print(double(6))",
-        "package t
-
-        fun apply(f: fun (int) [int], x: int) [int | string | bool] {
-            return f(x)
-        }
-
-        fun main() {
-            print(apply(print, 5))
-        }",
-        "package config
-
-        import (
-            os,
-            strings,
-            time,
-            math
-        )
-
-        fun now() [time.Time] {
-            return time.Now
-        }
-
-        fun main() {
-            let content = os.ReadFile(\"log.txt\")
-            let res = os.ReadFile(\"x\") == 5
-
-            let time = now(5)
-        }
-
-        let x = 3 + 2",
-        "package Point
-
-        struct Point {
-            x: int,
-            y: int
-        };
-
-        fun getX(p: Point) [int] {
-            return p.x + 1;
-        }
-
-        fun getXInCondition(p: Point) [int] {
-            if p.x > 5 {
-                return p.x
-            }
-        }
-
-        fun getNotX(p: Point) [int] {
-            return p.i
-        }
-
-        fun getXNotFromPoint(p: Point) [int] {
-            return o.x
-        }
-
-        fun getXFromPrimitive(p: int) [int] {
-            return p.x
-        }",
-    ];
-
-    for input in inputs {
-        println!("{}", check(input));
+    if args.len() < 2 {
+        println!("Error: Missing required arguments. Expected at least 2 arguments, but received {}\n\nUsage: violette <build|run> <file.vio>", args.len());
+        return;
     }
+
+    let command = &args[1];
+    let file = &args[2];
+
+    compile(command, file);
 }
